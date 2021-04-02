1ca19e9a669226c42d4e9dc375e590cbbc0e6763
JToTl <80580108+JToTl@users.noreply.github.com>
2021-04-02 14:58:35 +0900

    Add files via upload

---
+++ src/main/java/battleroyale/GlobalClass.java
@@ -0,0 +1,8 @@
+package battleroyale;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class GlobalClass {
+    public static BattleRoyaleData runningGame;
+    public static CustomConfig editedConfig;
+}
---
+++ src/main/java/battleroyale/Main.java
@@ -0,0 +1,20 @@
+package battleroyale;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        CustomConfig config=new CustomConfig(this);
+        config.saveDefaultConfig();
+        new EventList(this);
+        getCommand("battleroyale").setExecutor(new Commands());
+        getLogger().info("バトロワプラグインが有効になりました");
+    }
+
+    @Override
+    public void onDisable(){
+        getLogger().info("バトロワプラグインが無効になりました");
+    }
+}
