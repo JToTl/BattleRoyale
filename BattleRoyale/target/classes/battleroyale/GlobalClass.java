ad8968e74686f553a390d3a3a52a9ac13ecc829f
JToTl <80580108+JToTl@users.noreply.github.com>
2021-04-02 14:54:43 +0900

    なんだこれは！

--- BattleRoyale/target/classes/battleroyale/GlobalClass.java
+++
@@ -1,8 +0,0 @@
-package battleroyale;
-
-import org.bukkit.configuration.file.FileConfiguration;
-
-public class GlobalClass {
-    public static BattleRoyaleData runningGame;
-    public static CustomConfig editedConfig;
-}
