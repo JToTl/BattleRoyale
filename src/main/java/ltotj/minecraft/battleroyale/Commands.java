df6f91bada3cff051c4e1d6caeaeaa6c920138a3
tororo_1066 <71592738+tororo1066@users.noreply.github.com>
2021-08-14 20:40:18 +0900

    1.17に対応 worldboaderが見えない？

--- src/main/java/ltotj/minecraft/battleroyale/Commands.java
+++ src/main/java/ltotj/minecraft/battleroyale/Commands.java
@@ -8,7 +8,6 @@ import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
